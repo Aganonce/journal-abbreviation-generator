10f6c819ad769c8216fd2963d789db6620d14c0b
Aganonce <james.flamino@newnetwork.cc>
2021-11-01 17:30:36 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# Journal Abbreviation Generator
+
+A simple program for quickly and accurately abbreviating all the journals and conferences inside of a .bib file. All abbreviations follow the guidelines of Standard Journal Abbreviation [(ISO4)](https://en.wikipedia.org/wiki/ISO_4). What is required is that the bibliographies contained within the target .bib file follow BibTex format. Consider the example below.
+
+```bibtex
+@article{flamino2021creation,
+  title={Creation, evolution, and dissolution of social groups},
+  author={Flamino, James and Szymanski, Boleslaw K and Bahulkar, Ashwin and Chan, Kevin and Lizardo, Omar},
+  journal={Scientific Reports},
+  volume={11},
+  number={1},
+  pages={1--11},
+  year={2021},
+  publisher={Nature Publishing Group}
+}
+```
+
+This program will swap **Scientific Reports** with **Sci. Rep.** and output the BibTex reference with the changes made. This can be done automatically for as many items as needed, and covers both `journal={}` and `booktitle={}` calls.
+
+## Setup
+
+After cloning the repository call
+
+```bash
+pip install -r requirements.txt
+```
+
+This will install all required dependencies. To run the abbreviation program, call
+
+```bash
+python abbreviate_journals.py <fname>
+```
+
+Where `<fname>` is the name of the input file. The program will automatically abbreviate all entries within and output the results to the same directory with an added **_abbrev** descriptor to the outputted filename. Note that the input file does not actually need to have a `.bib` file extension for the program to work.
+
+## Updating Abbreviations
+
+After the abbreviations have been made, the program will alert you to words that had no clear abbreviation, as well as journals that could not be correctly abbreviated. For the former problem, you can add additional abbreviations to the YAML file found at `abbreviations/abbrevs.yml` to help the program find the correct abbreviations. For the latter problem, manual abbreviations are required.
+
+To update the YAML file, follow the example below.
+
+```yaml
+Full word: Abbreviation.
+```
+
+The full word must be a singular word with the first letter capitalized, and the abbreviation must end in a period and also have its first letter capitalized.
---
+++ abbreviate_journals.py
@@ -0,0 +1,151 @@
+# Author: James Flamino (2021)
+
+import string
+import spacy
+import re
+import sys
+import yaml
+
+nlp = spacy.load('en_core_web_sm')
+missing_abbrevs = []
+missing_journals = []
+
+with open("abbreviations/abbrevs.yml", "r") as stream:
+    try:
+        abbrev_map = yaml.safe_load(stream)
+    except yaml.YAMLError as exc:
+        print(exc)
+        sys.exit()
+
+def contains_number(value):
+    for character in value:
+        if character.isdigit():
+            return True
+    return False
+
+def abbreviate_journal(res):
+    if res.lower() == 'plos one':
+        return 1
+
+    if res.lower() == 'science':
+        return 1
+
+    if 'arxiv' in res.lower():
+        return 1
+
+    for i in res:
+        if i in '!"#$%\'()*,-./;<=>?@[]^_`{|}~':
+            return 0
+    
+    res = re.sub(r'[^\w\s]','',res)
+    res = res.replace('&', '').replace('\\', '')
+    doc = nlp(res)
+
+    token_list = []
+    for token in doc:
+        if token.pos_ != 'ADP' and token.pos_ != 'DET' and token.pos_ != 'CCONJ' and token.pos_ != 'PRON':
+            token_list.append(token.text)
+
+    final_list = []
+    for token in token_list:
+        token_cap = token.capitalize()
+        if len(token) > 1:
+            if token_cap in abbrev_map:
+                token = abbrev_map[token_cap]
+            else:
+                if not contains_number(token):
+                    missing_abbrevs.append(token.capitalize())
+            final_list.append(token)
+        else:
+            if token.isalnum():
+                final_list.append(token)
+
+    output = ' '.join(final_list)
+    output = output.strip()
+
+    return output
+
+if __name__ == '__main__':
+    # PARAMETERS
+    if len(sys.argv) > 1:
+        fname = sys.argv[1]
+    else:
+        print('ERROR: This program requires one command line argument: fname')
+        sys.exit()
+
+    fname_list = fname.split('.')
+    oname = '.'.join(fname_list[:-1]) + '_abbrev.' + fname_list[-1]
+    print('\033[1mSaving abbreviated file to:\033[0m', oname)
+
+    w = open(oname, 'w')
+    with open(fname, 'r') as ins:
+        for line in ins:
+            line = line.rstrip('\n')
+            line = line.strip()
+            if 'title' in line and 'journal' in line:
+                res = re.findall(r'journal=\{.*?\}', line)[0]
+                res = res.replace('journal={', '').replace('}', '')
+                abbrev_res = abbreviate_journal(res)
+                if abbrev_res != 0 and abbrev_res != 1:
+                    line = line.replace(res, abbrev_res)
+                    w.write(line + '\n')
+                else:
+                    if abbrev_res == 0:
+                        w.write(line + '\n')
+                        missing_journals.append(res)
+                    elif abbrev_res == 1:
+                        w.write(line + '\n')
+
+            else:
+                if line[:7] == 'journal' or line[:9] == 'booktitle':
+                    if '{' in line and '}' in line:
+                        res = re.findall(r'\{.*?\}', line)[0]
+                        res = res.replace('{', '').replace('}', '')
+                    elif '"' in line:
+                        res = re.findall(r'"([^"]*)"', line)[0]
+                        res = res.replace('"', '')
+                    abbrev_res = abbreviate_journal(res)
+                    if abbrev_res != 0 and abbrev_res != 1:
+                        if line[:7] == 'journal':
+                            w.write('  journal={' + abbrev_res + '},\n')
+                        elif line[:9] == 'booktitle':
+                            w.write('  booktitle={' + abbrev_res + '},\n')
+                    else:
+                        if abbrev_res == 0:
+                            w.write('  ' + line + '\n')
+                            missing_journals.append(res)
+                        elif abbrev_res == 1:
+                            w.write('  ' + line + '\n')
+                else:
+                    if len(line) > 0:
+                        if line[0] == '@' or line[0] == '}':
+                            w.write(line + '\n')
+                        else:
+                            w.write('  ' + line + '\n')
+                    else:
+                        w.write('\n')
+    w.close()
+
+    print('===================================================')
+    print('\033[1mWords that do not have abbreviations (update YAML):\033[0m')
+    missing_abbrevs = set(missing_abbrevs)
+    for item in missing_abbrevs:
+        print(item)
+    print('===================================================')
+    print('\033[1mJournals that could not be properly abbreviated:\033[0m')
+    missing_journals = set(missing_journals)
+    for item in missing_journals:
+        period_count = item.count('.')
+        if period_count > 1:
+            has_other_punctuation = False
+            new_item = item.replace('.', '')
+            for i in new_item:
+                if i in string.punctuation:
+                    has_other_punctuation = True
+            if not has_other_punctuation:
+                print(item, '\033[1m(This journal may already be abbreviated.)\033[0m')
+            else:
+                print(item)
+        else:
+            print(item)
+    print('===================================================')
